7243e43405e5e6883d44b528bbac5ca8b1a924e0
Keloran <keloran@develbox.info>
2024-06-30 22:29:20 +0100

    refactor: Simplify path for retrieving remote secret

--- vault_test.go
+++ vault_test.go
@@ -133,7 +133,7 @@ func TestGetSecret(t *testing.T) {
 	assert.Nil(t, err)
 
 	v := NewVault(address, "MyToKeN").SetContext(ctx)
-	err = v.GetRemoteSecrets("kv/data/secret/test1")
+	err = v.GetRemoteSecrets("secret/test1")
 	assert.Nil(t, err)
 
 	secret, err := v.GetSecret("foo1")
